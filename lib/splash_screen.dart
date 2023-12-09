f44bf5d32ec8df95c22cfcb064cf2ec5ed30a3ef
CrongCrong23 <eugenelopez.montesor@bicol-u.edu.ph>
2023-12-09 10:12:11 +0800

    Splash Screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.purple])),
+      ),
+    );
+  }
+}
